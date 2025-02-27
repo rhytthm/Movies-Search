411947768ac454d0a7fde8a7bc82428128b1b65c
Rhytthm <rhytthm1717@gmail.com>
2025-02-27 19:29:25 +0530

    Initial Commit

---
+++ MovieSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieSearch
+//
+//  Created by RAJEEV MAHAJAN on 27/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MovieSearchTests/MovieSearchTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSearchTests.swift
+//  MovieSearchTests
+//
+//  Created by RAJEEV MAHAJAN on 27/02/25.
+//
+
+import Testing
+@testable import MovieSearch
+
+struct MovieSearchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
